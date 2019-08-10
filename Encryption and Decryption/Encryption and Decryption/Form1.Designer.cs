e2dcbab22c2fc9c7484ba02ce32a5b965fb0ffc0
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-08-11 01:51:07 +0500

    "Secure conversation" (Encryption and decryption)
    Plain text

---
+++ Encryption and Decryption/Encryption and Decryption/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Encryption_and_Decryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(481, 99);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(110, 29);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Encrypt";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(637, 99);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(111, 29);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Decrypt";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(427, 278);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(379, 37);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(544, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(345, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Text";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(345, 278);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Hash";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(427, 151);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(379, 106);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Navy;
+            this.label3.Location = new System.Drawing.Point(255, 441);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(371, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Copyright all rights reserved CMT Softwares Solution";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Secure_conversation.Properties.Resources.refresh_icon_25;
+            this.pictureBox2.Location = new System.Drawing.Point(809, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(64, 60);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Secure_conversation.Properties.Resources.Encrypted_Messaging_Flat_Icon;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 99);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(276, 245);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 466);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Encryption and Decryption";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Encryption and Decryption/Encryption and Decryption/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace Encryption_and_Decryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MySqlConnection con;
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Enabled = true;
+            textBox2.Enabled = false;
+            textBox2.ReadOnly = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked==true)
+            {
+                // Numbers
+                Random r = new Random();
+                int[] num = new int[5];
+
+                for (int i = 0; i < num.Length; i++)
+                {
+                    num[i] = r.Next(1000,9999);
+                }
+
+
+                // Characters a - z
+                Random r2 = new Random();
+                string pin = "";
+
+                for (int j = 0; j < 5; j++)
+                {
+                    int[] num2 = new int[2];
+
+                    for (int i = 0; i < num2.Length; i++)
+                    {
+                        num2[i] = r2.Next(97, 123);
+
+                        pin += (char)num2[i];
+                    }
+                }
+
+                string val1 = pin.Substring(0, 2);
+                string val2 = pin.Substring(2, 2);
+                string val3 = pin.Substring(4, 2);
+                string val4 = pin.Substring(6, 2);
+                string val5 = pin.Substring(8, 2);
+
+
+                string hash = val1 + "" + num[0] + "" + val2 + "" + num[1] + "" + val3 + "" + num[2] + "" + val4 + "" + num[3] + "" + val5 + "" + num[4];
+
+                textBox2.Text = hash;
+                textBox2.Enabled = true;
+                textBox2.ReadOnly = true;
+
+                //   Server=remotemysql.com;Database=2YZIINJ2tI;Uid=2YZIINJ2tI;Pwd=Ju6sOU6rxz;
+                con = new MySqlConnection("Server=remotemysql.com;Database=2YZIINJ2tI;Uid=2YZIINJ2tI;Pwd=Ju6sOU6rxz;");
+                con.Open();
+
+                MySqlCommand cmd = new MySqlCommand("insert into code(text,hash)values('" + richTextBox1.Text + "','" + textBox2.Text + "')", con);
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+                
+                
+            }
+            else if(radioButton2.Checked==true)
+            {
+                con = new MySqlConnection("Server=remotemysql.com;Database=2YZIINJ2tI;Uid=2YZIINJ2tI;Pwd=Ju6sOU6rxz;");
+                con.Open();
+
+                MySqlDataAdapter sadp = new MySqlDataAdapter("select * from code where hash='" + textBox2.Text + "'", con);
+                DataTable dt = new DataTable();
+                sadp.Fill(dt);
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    if(textBox2.Text==dt.Rows[i]["hash"].ToString())
+                    {
+                        richTextBox1.Text = dt.Rows[i]["text"].ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not found");
+                    }
+                }
+                con.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox2.ReadOnly = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Enabled = false;
+            textBox2.Enabled = true;
+            textBox2.ReadOnly = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            textBox2.Clear();
+        }
+    }
+}
