58d46f00fe0d91452e97c6741718bbce6a22fa11
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-08-12 14:58:00 -0700

    Update Form1.Designer.cs

--- Encryption and Decryption/Encryption and Decryption/Form1.Designer.cs
+++ Encryption and Decryption/Encryption and Decryption/Form1.Designer.cs
@@ -22,6 +22,8 @@
 
         #region Windows Form Designer generated code
 
+            
+            
         /// <summary>
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
